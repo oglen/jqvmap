05d91105716cbde1529a2eb6b5f0096863cd5878
IsaacFang <isaac.fang@grapecity.com>
2014-07-12 14:32:40 +0800

    Advanced Pins

---
+++ data/pins.js
@@ -0,0 +1,59 @@
+var pins = {
+    us: {
+        name: 'America',
+        img: '',
+        links: [
+            {
+                text: 'aaa',
+                href: '#'
+            },
+            {
+                text: 'aaa',
+                href: '#'
+            },
+            {
+                text: 'aaa',
+                href: '#'
+            },
+            {
+                text: 'aaa',
+                href: '#'
+            },
+            {
+                text: 'aaa',
+                href: '#'
+            }
+        ]
+    },
+    cn: {
+        name: 'China',
+        img: '',
+        links: [
+            {
+                text: 'bbb',
+                href: '#'
+            },
+            {
+                text: 'bbb',
+                href: '#'
+            },
+            {
+                text: 'bbb',
+                href: '#'
+            },
+            {
+                text: 'bbb',
+                href: '#'
+            },
+            {
+                text: 'bbb',
+                href: '#'
+            }
+        ]
+    },
+    unknown: {
+        name: 'Unknown',
+        img: '',
+        links: []
+    }
+};
